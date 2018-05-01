d583674ff3b3d8467149cfb6b588749414fcfc80
vinothinisubramanian <37336263+vinothinisubramanian@users.noreply.github.com>
2018-05-01 09:45:18 +0530

    Create DATABASES &TABLES.sql

---
+++ beginner/DATABASES &TABLES.sql
@@ -0,0 +1,5 @@
+create database students;
+show databases;
+use students;
+create table details(name varchar(25),rollno varchar(8),college char(20),dept char(9),email char(20),marks varchar(5),subject char(25),timetable varchar(12),date varchar(7),time varchar(10));
+describe details;
